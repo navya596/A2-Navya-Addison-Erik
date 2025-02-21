8b20d429c12e6559cf10851ae918f10f37c5a2a6
Eurikan <erikso16@gmail.com>
2025-02-21 13:06:22 -0500

    added Direction enum

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Direction.java
@@ -0,0 +1,8 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public enum Direction {
+    EAST,
+    SOUTH,
+    WEST,
+    NORTH
+}
--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
@@ -1,6 +1,6 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
 public class Drone {
-
-
+    private double battery_level;
+    private Direction heading;
 }
